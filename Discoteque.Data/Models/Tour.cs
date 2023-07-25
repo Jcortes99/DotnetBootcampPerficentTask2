c87be7c810c79dba531438e909c9bbf5fbf55a4d
jcortes-intecol <jcortes@intecol.com.co>
2023-07-25 13:53:45 -0500

    Update song an tour models

---
+++ Discoteque.Data/Models/Tour.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Discoteque.Data.Models;
+
+public  class Tour : BaseEntity<int>
+{
+    /// <summary>
+    /// Name of the Song
+    /// </summary>
+    public string Name { get; set; } = "";
+    
+    /// <summary>
+    /// City name
+    /// </summary>
+    public string City { get; set; } = "";
+
+    /// <summary>
+    /// Is the tour SoldOut?
+    /// </summary>
+    public bool IsSoldOut { get; set; } = false;
+    
+    /// <summary>
+    /// The <see cref="Artist"/> id this Tour belongs to
+    /// </summary>
+    /// <value></value>
+    [ForeignKey("Id")]
+    public int ArtistId { get; set; }
+
+    /// <summary>
+    /// The <see cref="Artist"/> Entity this album is refering to
+    /// </summary> <summary>
+    public virtual Artist? Artist { get; set; } 
+}
